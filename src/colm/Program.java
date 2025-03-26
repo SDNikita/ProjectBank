bb5b60a09722df00774dd4b1b46299558d468571
SDNikita <sidnevnikitait@gmail.com>
2025-03-26 13:37:57 +0300

    Создание тестого графического окна

---
+++ src/colm/Program.java
@@ -0,0 +1,16 @@
+import javax.swing.*; // подключаем все средства java Swing
+
+public class Program { // класс с методом main()
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("НеОбманБанк"); // Для окна нужна "рама" - Frame
+        // стандартное поведение при закрытии окна - завершение приложения
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(300, 300); // размеры окна
+        frame.setLocationRelativeTo(null); // окно - в центре экрана
+        JButton button = new JButton("Снять деньги"); // Экземпляр класса JButton
+        // getContentPane() - клиентская область окна
+        frame.getContentPane().add(button); // Добавляем кнопку на Frame
+        frame.setVisible(true); // Делаем окно видимым
+    }
+}
